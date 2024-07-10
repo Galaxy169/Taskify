ed1e0d1e59d6c36ea19e6a7dedd7548d29a131ca
Aadil Ansari <adilansari689@gmail.com>
2024-07-10 18:13:52 +0530

    Made changes in Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,8 @@ const Home = () => {
   return (
     <>
       <div>
-        <div className="flex justify-center text-white bg-slate-600 m-6 p-6 rounded-3xl">
+        <div className="flex justify-center flex-col items-center text-white bg-slate-600 m-6 p-6 rounded-3xl gap-5">
           <h1>Home Page (Under Construction)</h1>
-        </div>
-
-        <div className="flex justify-center text-white bg-slate-600 m-6 p-6 rounded-3xl">
           <Link to="/yourtasks" className="text-teal-400 hover:text-green-500 hover:underline hover:text-[17px] transition-all duration-700 ease-out">
             Create Your To Do List
           </Link>
