2734a14b4dd3659748e5601908e8e5619983cb3a
Aadil Ansari <adilansari689@gmail.com>
2024-07-10 18:10:03 +0530

    Added Active Style to Nav Bar, Added Tailwind Active Directive for NavLink to simplify, Added Home Page Link to the Logo, Some Design Fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <nav className="bg-gray-600 rounded-lg">
         <div className="flex justify-between items-center">
-          <div className=" text-teal-400 hover:text-teal-100 hover:text-[26px] text-2xl mx-5 transition-all hover:shadow-sm hover:shadow-emerald-700 hover:rounded-3xl p-2.5 hover:bg-slate-950 cursor-pointer">
-            <span className="">Taskify</span>
+          <div className="text-teal-400 hover:text-teal-100 hover:text-[26px] text-2xl mx-5 transition-all hover:shadow-sm hover:shadow-emerald-700 hover:rounded-3xl p-2.5 hover:bg-slate-950 cursor-pointer">
+            <Link to="/" className="">Taskify</Link>
           </div>
           <div className="flex md:mx-2 text-teal-400 bg-slate-800 rounded-xl cursor-pointer">
-            <Link to="/" className="py-5 hover:bg-slate-900  hover:text-teal-100 rounded-l-xl  transition duration-400">
+            <NavLink to="/" className={(e)=>{return e.isActive ? "NavLinkActive rounded-l-xl" : "NavLink rounded-l-xl";}}>
               <li className="md:mx-5 mx-4 list-none">
                 Home
               </li>
-            </Link>
-            <Link to="/yourtasks"  className="py-5 hover:bg-slate-900 hover:text-teal-100 rounded-r-xl transition duration-400">
+            </NavLink>
+            <NavLink to="/yourtasks" className={(e)=>{return e.isActive ? "NavLinkActive rounded-r-xl" : "NavLink rounded-r-xl";}}>
               <li className="md:mx-5 mx-3 list-none">Your Tasks</li>
-            </Link>
+            </NavLink>
           </div>
         </div>
       </nav>
